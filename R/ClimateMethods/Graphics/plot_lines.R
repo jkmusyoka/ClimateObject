eb94cfaccc8e5a09d45381cc3e8cc4b607dd7d41
ntfredo <fredo@aims.ac.za>
2015-06-17 18:52:11 +0300

    Plot_lines method with ggplot for multiple variables.

--- R/ClimateMethods/Graphics/plot_lines.R
+++ R/ClimateMethods/Graphics/plot_lines.R
@@ -1,64 +1,66 @@
-# Still have to generalize this method to be able to plot more than two variables. By now it fiiting well with SIAC-practical
-# assignmet topic 5.
-# need to fix the legend for each plot
+# After looking and findout that the function ggplot can plot multiple columns on the same plot,we can adopt it for multiple variables
+#=========================================================================================================
 
-
-climate$methods(plot_lines = function(data_list=list(), var1, var2, col1 = "blue", type1 = "h",type2="p",
-                                                col2 = "red", col3 = "green", xlabel = "Year",lwd=2, finite = TRUE,
-                                                pch1 = 1, pch2 = 1, pch3 = 1, main=c(),  time_period = yearly_label){    
-  
+climate$methods(plot_lines = function(data_list=list(), interest_var, data_period_label = yearly_label)
+{   
+  require(ggplot2)
+  require(reshape)
   
   # get_climate_data_objects returns a list of the climate_data objects specified
   # in the arguments.
-  # If no objects specified then all climate_data objects will be taken by default
-  # the var1 and var2 must be label. 
-   data_list = add_to_data_info_required_variable_list(data_list, list( var1 )) 
-   data_list = add_to_data_info_required_variable_list(data_list, list( var2 ))
-  # we should be able to specify the time period.
-   data_list = add_to_data_info_time_period(data_list, time_period) 
+  # If no objects specified then interest_var climate_data objects will be taken by default
   
-   climate_data_objs_list = get_climate_data_objects(data_list)
-
+  # Can I use a list of variables? Yes . 
+  # The analysis should take account of the structure of the data.
+  data_list = add_to_data_info_required_variable_list(data_list, interest_var) 
+  data_list = add_to_data_info_time_period(data_list, data_period_label) 
+  
+  climate_data_objs_list = get_climate_data_objects(data_list)
+  
   for(data_obj in climate_data_objs_list) {
-     # get the columns of interest for the plot.
-    var1 = data_obj$getvname( var1 )
-    var2 = data_obj$getvname( var2 )
     
-    date_col = data_obj$getvname( date_label )
-    #adding year column if not present 
-    if( !(data_obj$is_present( year_label ) ) ) {
-      data_obj$add_year_month_day_cols() 
+    # we need to get the column of interest for the plot.
+    # since the column of interest is a list, the loop gets interest_var at the same time.
+    interest_variable =list()
+    for(i in 1:length(interest_var)){
+      
+      interest_variable[[i]] <- data_obj$getvname(interest_var[[i]]) 
     }
-    year_col = data_obj$getvname( year_label )
+    #print(interest_variable)
+    
+    
+    date_col = data_obj$getvname(date_label)
+    
+    #adding year column if not present 
+    if( !(data_obj$is_present(year_label)) ) {
+      data_obj$add_year_col()
+    }
+    year_col = data_obj$getvname(year_label)
     
     curr_data_list = data_obj$get_data_for_analysis(data_list)
     
-#     #Check will be done here. var1 is a list
-#     l = length(var1)
-#     #Start for loop for each variable entered here
-#     for( i in  1: l ){
-#       
-#       plot( xaxis[[ i ]], var1[[ i ]], type = "c", col= col3[[i]],
-#             ylim = c( range( var1[[ i ]], na.rm = TRUE)+ 20 ), lty = lty2[[ i ]],
-#             main = main[[ i ]], ylab = ylab[[ i ]], xlab = xlab[[ i ]])
-#     }
-    
     for( curr_data in curr_data_list ) {
-       # plotting the first plot. 
-      plot(curr_data[[ year_col ]], curr_data[[var1]], type = type1, lwd = lwd, col = col1, xlab = xlabel,main = main,
-            ylim = c( range( curr_data[[var1]], curr_data[[var2]], finite = finite) ))
-      #Adding points to the plot
-      lines(curr_data[[ year_col ]], curr_data[[var1]], type=type2, col=col2, pch = pch1)
-      #Adding the second plot
-      points(curr_data[[ year_col ]], curr_data[[var2]], type = type1, col=col3, pch = pch2 )
-      #Adding points to the second plot
-      lines(curr_data[[ year_col ]], curr_data[[var2]], type=type2, col=col2, pch = pch1)
+      # subset the data. Here get only time period and the interest variables 
+      dat <- subset(curr_data, select=c( year_col, interest_variable = as.character(interest_variable)))
+      print(head(dat))
+      #Melt the data into a form suitable for easy casting
+      dat2 <- melt(dat ,  id = 'Year')
+      #       print(head(dat2))
+             print(names(dat2))
+      dat2$Year <-as.factor(dat2$Year) # factor
+      dat2$value <- as.integer(dat2$value) # integer
+      #       print(class(dat2$Year))
+      #       print(class(dat2$variable))
+      #       print(class(dat2$value))
+      #"Year"     "variable" "value"
+      # plot interest_var variables on the same graph
+      # Need to read more about ggplot bcse here it is not plotting.
+      #?ggplot() is typically used to construct a plot incrementally.
+      ggplot(data = dat2, aes(x = Year, y = value, group=1)) + geom_line(aes(colour = variable))+
+        ggtitle("Start of the Rain by Year")
       
     }
+    
   }
 }
 )
-
-
-
-
