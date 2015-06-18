b0de48f7cfe888d29539bf726e0270a59d156d06
aduut <k.aduut@gmail.com>
2015-06-18 16:19:00 +0000

    removed some print statements and reorganized the add some if statements to all for sort flexiblity.

--- R/ClimateMethods/Graphics/plot_inventory_method.R
+++ R/ClimateMethods/Graphics/plot_inventory_method.R
@@ -16,8 +16,6 @@ climate$methods(plot_inventory = function (data_list=list(),ylab,na.rm=TRUE,col=
   data_list = add_to_data_info_required_variable_list(data_list, list(var_label, station_label, date_label))
   # Merge the data into a single data frame so that we can do a by station analysis.
   data_list[[merge_data_label]]=TRUE
-  
-  print(data_list)
   # time period and station
   data_list = add_to_data_info_time_period(data_list, daily_label)
   
@@ -33,36 +31,35 @@ climate$methods(plot_inventory = function (data_list=list(),ylab,na.rm=TRUE,col=
     }
     
     curr_data = data_obj$get_data_for_analysis(data_list)
-    
     #Create binary field indicating whether variable of interest (Rain) is missing or non-missing
     curr_data$val<-as.numeric(is.na(curr_data[[var_col]]))
-    
     #Stations will be plotted from bottom to top but we want alphatically first to be on the top so sort stations into reverse alphabetical order. 
     curr_data<-curr_data[rev(order(curr_data[[station_col]])),]
-    
     #reshape data into 1 row per day, 1 column per station, with values as calculated previously
     curr_data<-reshape(curr_data[,c(station_col,date_col,"val")],timevar=station_col,idvar=date_col,v.names="val",direction="wide")
-    
     #where value is NA after reshape the station did not have a row for that date in the input - this is also missing data so overwrite accordingly.
     curr_data[is.na(curr_data)]<-1
-    
     #sort by date
     curr_data<-curr_data[order(curr_data[[date_col]]),]
-    
+    print(curr_data)
     #set plot window  
     par = graph_parameter 
-     
+    print("1")
+    if(sort==FALSE){
     # for the plot 
     image(x=curr_data[[date_col]],y=1:(ncol(curr_data)-1),as.matrix(curr_data[,-1]),yaxt="n",ylab="",col,xlab="",main= c( data_name, main_title))
     #add white spaces to help delineate the groups
     segments(x0=min(curr_data[[date_col]]),x1=max(curr_data[[date_col]]),y0=seq(0.5,ncol(curr_data)+0.5,by=1),col="white")
-    
     #add labels
     text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),levels(curr_data[[station_col]]),xpd=T,pos=2,cex=0.75)
     
-    if(sort==TRUE){
-    #add labels
-    text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),rev(sort(levels(curr_data[[station_col]]))),xpd=T,pos=2,cex=0.75)
+    }else{ 
+      image(x=curr_data[[date_col]],y=1:(ncol(curr_data)-1),as.matrix(curr_data[,-1]),yaxt="n",ylab="",col,xlab="",main= c( data_name, main_title))
+      #add white spaces to help delineate the groups
+      segments(x0=min(curr_data[[date_col]]),x1=max(curr_data[[date_col]]),y0=seq(0.5,ncol(curr_data)+0.5,by=1),col="white")
+      print("3")
+      #add labels
+      text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),rev(sort(levels(curr_data[[station_col]]))),xpd=T,pos=2,cex=0.75)
     }
   }
 }
