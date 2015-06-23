ac68e274545b3e86a355332e92152be1c3da3f6e
ntfredo <fredo@aims.ac.za>
2015-06-23 09:58:15 +0300

    Modified plot_multiple_lines method.

--- R/ClimateMethods/Graphics/plot_lines.R
+++ R/ClimateMethods/Graphics/plot_lines.R
@@ -23,17 +23,15 @@
 
 climate$methods(plot_multiple_lines = function(data_list=list(), variables, col = c("blue", "red", "green"), type = c("h", "h", "h"), lty= c(1,2,3), lty_points= c(1,2,3), ylabel="Observations", xlabel = "Year",lwd=c(2), 
                                       lwd_points=c(2,2,2), pch = c(2,20,4),bty = "o", main="Vertical Lines", time_period = yearly_label, legend.location = rep(list("topright"), length(variables)), 
-                                      legend=rep(list(c("1", "2","3")), length(variables)), legend_text_width = strwidth("0.001") ){    
+                                      legend=rep(list(c("1", "2","3")), length(variables)), legend_text_width = strwidth("0.001"), na.rm=TRUE ){    
   
-  # get_climate_data_objects returns a list of the climate_data objects specified
-  # in the arguments.
-  # If no objects specified then all climate_data objects will be taken by default
+  # get_climate_data_objects returns a list of the climate_data objects specified in the arguments.
+  # If no objects specified then all climate_data objects will be taken by default.
     
   if (!is.list(variables)){
     interest_var = list(variables)
   }
   data_list = add_to_data_info_required_variable_list(data_list,  variables) 
-  
   # we should be able to specify the time period.
   data_list = add_to_data_info_time_period(data_list, time_period) 
   #Get the data objects
@@ -45,21 +43,19 @@ climate$methods(plot_multiple_lines = function(data_list=list(), variables, col 
   for(data_obj in climate_data_objs_list) {
     #get the name of the data set
     data_name = data_obj$get_meta(data_name_label)
-    
-    #adding year column if not present and if date present
+        
+    #adding year column if not present 
     if( !(data_obj$is_present( year_label ) ) ) {
-      date_col = data_obj$getvname( date_label )
       data_obj$add_year_month_day_cols() 
     }
     year_col = data_obj$getvname( year_label )
     
-    #Get the data frames present in data_obj 
     curr_data_list = data_obj$get_data_for_analysis(data_list)
     
     for( curr_data in curr_data_list ) {
       #Plot multiple line at once
       matplot( curr_data[[year_col]], curr_data[interest_var[[j]]], col=col, lwd=lwd, type=type, lty=lty, xlab=xlabel,ylab=ylabel,main=c(data_name, main),
-               ylim=c( range( curr_data[interest_var[[j]]], na.rm=TRUE) ))
+               ylim=c( range( curr_data[interest_var[[j]]], na.rm=na.rm) ))
       
       #Add points on top of the lines
       matpoints(curr_data[[ year_col ]], curr_data[ interest_var[[ j ]] ], type = "p", pch = pch,  col=col, lwd=lwd_points[[j]], lty=lty_points)
