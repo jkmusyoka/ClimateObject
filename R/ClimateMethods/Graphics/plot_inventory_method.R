e896466d3c73f3dcdc9095efd09e0df02fbc78a6
aduut <k.aduut@gmail.com>
2015-07-02 14:00:12 +0000

    Change the layout of the function for a more efficient computations for sort variable.Still, the sort equals to true case is not working well. Data is not flipping around in the same manner as station labels are.

--- R/ClimateMethods/Graphics/plot_inventory_method.R
+++ R/ClimateMethods/Graphics/plot_inventory_method.R
@@ -34,33 +34,52 @@ climate$methods(plot_inventory = function (data_list=list(),ylab,na.rm=TRUE,col=
     curr_data = data_obj$get_data_for_analysis(data_list)
     #Create binary field indicating whether variable of interest (Rain) is missing or non-missing
     curr_data$val<-as.numeric(is.na(curr_data[[var_col]]))
-    station_list<-levels(curr_data$Station)
+    station_list<-levels(curr_data[[station_col]])
+    View(curr_data)
     if(sort==TRUE){
       #Stations will be plotted from bottom to top but we want alphatically first to be on the top so sort stations into reverse alphabetical order. 
       curr_data<-curr_data[rev(order(curr_data[[station_col]])),]
+      
+      #reshape data into 1 row per day, 1 column per station, with values as calculated previously
+      curr_data<-reshape(curr_data[,c(station_col,date_col,"val")],timevar=station_col,idvar=date_col,v.names="val",direction="wide")
+    
+      #where value is NA after reshape the station did not have a row for that date in the input - this is also missing data so overwrite accordingly.
+      curr_data[is.na(curr_data)]<-1
+      #sort by date
+      curr_data<-curr_data[order(curr_data[[date_col]]),]
+      #set plot window  
+      par = graph_parameter 
+      
+      image(x=curr_data[[date_col]],y=1:(ncol(curr_data)-1),z=as.matrix(curr_data[,-1]),yaxt="n",ylab="",col=c("blue", "red"),xlab="",main= c( data_name, main_title))
+      #add white spaces to help delineate the groups
+      segments(x0=min(curr_data[[date_col]]),x1=max(curr_data[[date_col]]),y0=seq(0.5,ncol(curr_data)+0.5,by=1),col="white")
+      #add labels
+      text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),rev(sort(station_list)),xpd=T,pos=2,cex=0.75)
+      
     }
+    
+    if(sort==FALSE){
     #reshape data into 1 row per day, 1 column per station, with values as calculated previously
     curr_data<-reshape(curr_data[,c(station_col,date_col,"val")],timevar=station_col,idvar=date_col,v.names="val",direction="wide")
+    print("3")
     #where value is NA after reshape the station did not have a row for that date in the input - this is also missing data so overwrite accordingly.
     curr_data[is.na(curr_data)]<-1
     #sort by date
     curr_data<-curr_data[order(curr_data[[date_col]]),]
     #set plot window  
     par = graph_parameter 
-    if(sort==FALSE){
+    
+    
     # for the plot 
     image(x=curr_data[[date_col]],y=1:(ncol(curr_data)-1),as.matrix(curr_data[,-1]),yaxt="n",ylab="",col=c("blue", "red"),xlab="",main= c( data_name, main_title))
     #add white spaces to help delineate the groups
     segments(x0=min(curr_data[[date_col]]),x1=max(curr_data[[date_col]]),y0=seq(0.5,ncol(curr_data)+0.5,by=1),col="white")
     #add labels
     text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),station_list,xpd=T,pos=2,cex=0.75)
-    }else{ 
-      image(x=curr_data[[date_col]],y=1:(ncol(curr_data)-1),z=as.matrix(curr_data[,-1]),yaxt="n",ylab="",col=c("blue", "red"),xlab="",main= c( data_name, main_title))
-      #add white spaces to help delineate the groups
-      segments(x0=min(curr_data[[date_col]]),x1=max(curr_data[[date_col]]),y0=seq(0.5,ncol(curr_data)+0.5,by=1),col="white")
-      #add labels
-      text(x=min(curr_data[[date_col]]),y=1:(ncol(curr_data)-1),rev(sort(station_list)),xpd=T,pos=2,cex=0.75)
-      }
+    
+    
+    } 
+      
     }
   }
 )
