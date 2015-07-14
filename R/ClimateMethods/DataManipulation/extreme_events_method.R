2c713c20681372c10d08075d2cfaff5c3b0ccb08
Fanuel Omondi Otieno <fanuel@aims.ac.za>
2015-07-14 16:49:19 +0300

    modified extreme_events method

--- R/ClimateMethods/DataManipulation/extreme_events_method.R
+++ R/ClimateMethods/DataManipulation/extreme_events_method.R
@@ -1,5 +1,5 @@
 climate$methods(extreme_events=function(data_list=list(), year, required_var=rain_label,na.rm=TRUE,max_min=TRUE,extreme=max,sum_day=1,val_threshold=FALSE,
-                                        threshold_value=0,start_day=1, end_day=366){
+                                        threshold_value=0,start_day=1, end_day=366, values_between=FALSE,lower_lim,upper_lim){
   
   #required variable
   data_list = add_to_data_info_required_variable_list(data_list, list(required_var))
@@ -43,12 +43,22 @@ climate$methods(extreme_events=function(data_list=list(), year, required_var=rai
       thresh=list()
       
       for (yr in year){
-        sub=subset(curr_data,curr_data[[season_col]]==yr & curr_data[[dos_col]]>=start_day & curr_data[[dos_col]]<=end_day)
         
+        if (values_between){
+          if (missing(upper_lim)){
+            upper_lim=max(curr_data[[rain_col]],na.rm=na.rm)
+          }
+          if (missing(lower_lim)){
+            lower_lim=min(curr_data[[rain_col]],na.rm=na.rm)
+          }
+          sub=subset(curr_data,curr_data[[season_col]]==yr & curr_data[[dos_col]]>=start_day & curr_data[[dos_col]]<=end_day & curr_data[[rain_col]]>= lower_lim & curr_data[[rain_col]]<= upper_lim)
+          }else{
+            sub=subset(curr_data,curr_data[[season_col]]==yr & curr_data[[dos_col]]>=start_day & curr_data[[dos_col]]<=end_day)
+            }
         val=rowSums(outer(1:(length(sub[[rain_col]])-sum_day+1),1:sum_day,FUN=function(i,j){sub[[rain_col]][(j - 1) + i]}),na.rm=na.rm)      
         
         if(max_min && !val_threshold){
-          doy=sub[[dos_col]][sum_day-1+which(val %in% max(val,na.rm=na.rm))]
+          doy=sub[[dos_col]][sum_day-1+which(val %in% extreme(val,na.rm=na.rm))]
           if (length(doy)>1){
             doy=min(doy) 
             }
