062b77bd5e15392434240a7c4dfe33d13fa209b1
kogos <stephen@aims.edu.gh>
2015-07-27 16:50:15 +0300

    Edited

--- R/ClimateMethods/DataManipulation/monthly_summaries_method.R
+++ R/ClimateMethods/DataManipulation/monthly_summaries_method.R
@@ -1,5 +1,4 @@
-climate$methods(monthly_summary=function( data_list=list(), threshold=0, whisklty=1, whiskcol="red", fill_col="blue", connect_median=FALSE, lty_median=1,
-                                                         col_median="black", lwd=1, title="Monthly Rainfall Amount",ylab="Rainfall (mm)",xlab="Month", na.rm=TRUE, summ = mean){
+climate$methods(monthly_summary=function( data_list=list(),upper_lim = " ", lower_lim = 0, start_day = 1, end_day = 366, threshold=0, na.rm=TRUE, summ = min, decimals = 2){
   #--------------------------------------------------------------------------------------------#
   # This function plots the boxplot of the daily rainfall observations per month for all the years
   #-------------------------------------------------------------------------------------------#
@@ -21,41 +20,38 @@ climate$methods(monthly_summary=function( data_list=list(), threshold=0, whisklt
     if( ! data_obj$is_present( month_label ) ){
       data_obj$add_year_month_day_cols()
     }
-    # Get the title of the column of months
-    month_col = data_obj$getvname(month_label)
-    rain_col =  data_obj$getvname(rain_label)
-    year_col = data_obj$getvname(year_label)
+    # Get titles of interested columns
+    month_col <- data_obj$getvname(month_label)
+    rain_col <-  data_obj$getvname(rain_label)
+    year_col <- data_obj$getvname(year_label)
+    doy_col <- data_obj$getvname(doy_label)
 
     # Access data in methods
-    curr_data_list = data_obj$get_data_for_analysis(data_list)
+    curr_data_list <- data_obj$get_data_for_analysis(data_list)
     for( curr_data in curr_data_list ) {
-      dat <- curr_data[curr_data[[rain_col]] > threshold, c(year_col, rain_col,month_col)]
-      #print(names(dat))
-      mon = month(dat[[month_col]], label=T)
-      dat[[month_col]] = mon
-  
-      #summary = summ(dat[[rain_col]], na.rm = na.rm)
-      #print(summary)
-      tabl = aggregate(dat[[rain_col]] ~ dat[[year_col]]+dat[[month_col]], data = dat, summ, na.rm= na.rm)
+      dat <- curr_data[(curr_data[[doy_col]] >= start_day & curr_data[[doy_col]] <= end_day), c(year_col, rain_col,month_col,doy_col)]
+
+      if(missing(upper_lim)){
+        upper_lim = max(dat[[rain_col]], na.rm = na.rm)
+      }
+      for (i in 1:nrow(dat)){
+        if(dat[[rain_col]][i]<lower_lim || dat[[rain_col]][i]>upper_lim){
+          dat[[rain_col]][i]<-NA
+        }
+      }
+      dat[[month_col]] <- month(dat[[month_col]], label=T)  
+      ss <- dcast(dat,  dat[[year_col]] ~ dat[[month_col]], summ, value.var = rain_col, na.rm = na.rm)
+      names(ss)[names(ss)=="dat[[year_col]]"] <- "Year"
+
+      for(i in 2:(ncol(ss))){
+        ss[,i] = round(ss[,i], decimals)
+        sa = which(ss[,i] %in% c(NA, -Inf, NaN))
+        ss[sa,i]<-"*"  
+      }
       
-      #print(names(tabl))
-      
-      #print(tabl)
-      digits=2
-      #ss = dcast(tabl, dat[[rain_col]]~dat[[month_col]] , fun.aggregate = function(x) as.character(x)[1])
-      ss = dcast( tabl, dat[[year_col]]~dat[[month_col ]], value.var = "dat[[rain_col]]" )
-      print(ss)
-      #print(unstack(tabl, tabl[,3] ~ tabl[,2]+tabl[,1]))
-#       if( connect_median == TRUE) {
-#         lines(  boxplot(dat[[rain_col]]~mon,whisklty=whisklty,whiskcol=whiskcol,col=fill_col,xlab=xlab,
-#                         ylab=ylab, main= c(data_name, title) )$stats[3,], col=col_median, lty=lty_median, lwd=lwd )
-#       }else{
-#         boxplot(dat[[rain_col]]~mon,whisklty=whisklty,whiskcol=whiskcol,col=fill_col,xlab=xlab,
-#                 ylab=ylab, main= c(data_name, title) )
-#       }
-      
+      return(ss)
     }
   }
   
 } 
-)  
+)
