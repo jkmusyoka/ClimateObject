5037d28d7b0109097a98e6c74a07ab119eaecef4
ntfredo <fredo@aims.ac.za>
2015-07-28 12:15:54 +0300

    Merged tables in display_daily method.

--- R/ClimateMethods/DataManipulation/display_daily_data_method.R
+++ R/ClimateMethods/DataManipulation/display_daily_data_method.R
@@ -18,7 +18,7 @@
 #' climateObj$display_daily()
 #' @return It returns tables list
 
-climate$methods(display_daily = function(data_list = list(), print_tables = FALSE,row.names = FALSE, variable = rain_label, months_list = month.abb, day_display = "Day"){
+climate$methods(display_daily = function(data_list = list(), print_tables = FALSE, row.names = FALSE, na.rm = TRUE, variable = rain_label, threshold = 0.85, months_list = month.abb, day_display = "Day"){
     
   #required variable
   data_list = add_to_data_info_required_variable_list(data_list, list(variable))
@@ -28,6 +28,9 @@ climate$methods(display_daily = function(data_list = list(), print_tables = FALS
   climate_data_objs = get_climate_data_objects( data_list )
   
   for( data_obj in climate_data_objs ) {
+    
+    curr_threshold = data_obj$get_meta(threshold_label,threshold)
+    
     #get required variable name
     interest_var = data_obj$getvname( variable )
     
@@ -46,6 +49,7 @@ climate$methods(display_daily = function(data_list = list(), print_tables = FALS
       # initialize tables as a list
       tables = list()
       tables_2 = list()
+      tables_12 = list()
       # Split curr_data into single data frames for each year
       # It returns a list of data.frames, splited by year 
       # This is much faster (6x faster when checked) than subsetting
@@ -54,6 +58,7 @@ climate$methods(display_daily = function(data_list = list(), print_tables = FALS
       # counter used in the loop
       i = 1
       j = 1
+      k = 1
       # loop through the splited data frames 
       for ( single_year in years_split ) {
         # produce table with data
@@ -64,32 +69,34 @@ climate$methods(display_daily = function(data_list = list(), print_tables = FALS
         colnames( tables[[i]] )[2:end] <- months_list[1:end-1]
         
         #create quick function to count number of obs larger than a certain value which can be run in an apply
-        largerthan <-function(x,val){
+        largerthan <- function(x,val){
           length(na.omit(x[x>val]))
         }
         #produce second table with summary stats
-        tables_2[[j]]<-rbind(colSums(tables[[j]][,-1],na.rm=T),apply(tables[[j]][,-1],2,max,na.rm=T),apply(tables[[j]][,-1],2,largerthan,val = 0.85))
-        ## add dimnames
-        tables_2[[j]]<-cbind(c("Total","Maximum","Number >0.85"),tables_2[[j]])
-        ## Making dataframe for second table
-        tables_2[[j]]=data.frame(tables_2[[j]])
-        
+        tables_2[[j]] <- rbind(colSums(tables[[j]][,-1], na.rm = na.rm), apply(tables[[j]][,-1],2, max, na.rm = na.rm), apply(tables[[j]][,-1],2,largerthan, val = curr_threshold))
+        # add dimnames
+        tables_2[[j]] <- cbind(c("Total","Maximum","Number >0.85"), tables_2[[j]])
+        # Making dataframe for second table
+        tables_2[[j]] <- data.frame(tables_2[[j]])
         #add dimnames for the first column.
-        colnames(tables_2[[j]])[1]<-("Day")
+        colnames(tables_2[[j]])[1] <- ("Day")
+        # merge the tables
+        tables_12[[k]] <- rbind(tables[[i]], tables_2[[j]])
         i = i + 1
         j = j + 1 
+        k = k + 1
                 
       }
       # The names of years_split is the list of years as strings.
       # These are better labels than numbers so they can be identified better
-      names( tables ) <- names( years_split )
-      rettables[[data_obj$get_station_data(curr_data, station_label)]]=tables  
+      names( tables_12 ) <- names( years_split )
+      rettables[[data_obj$get_station_data(curr_data, station_label)]] = tables_12  
     }
     # Only print if requested
-    if (print_tables) {print( tables, row.names = row.names) }
+    if (print_tables) {print( tables_12, row.names = row.names) }
         
     # Always return the tables list because If we don't return and don't print then the method does nothing!    
-    return( tables_2 )
+    return( tables_12 )
     }  
 }
 )
