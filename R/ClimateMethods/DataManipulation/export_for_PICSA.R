46f63e12300f353a4252cd948d636938cd82c4ab
aduut <k.aduut@gmail.com>
2015-10-06 10:08:22 +0000

    Deleted some print and View statements, and edited and uncommented line 37.

--- R/ClimateMethods/DataManipulation/export_for_PICSA.R
+++ R/ClimateMethods/DataManipulation/export_for_PICSA.R
@@ -19,34 +19,27 @@ climate$methods(export_for_PICSA =function(data_list = list(), month_start = c(5
 
 {  
       #first call the seasonal summary method with the right agruments
-      print("1")
       .self$seasonal_summary.rain(data_list = data_list, month_start = month_start, number_month =number_month, threshold = threshold, summaries = summaries, use_threshold_as_lower = use_threshold_as_lower, strict_threshold = strict_threshold,longest_dry_spell = longest_dry_spell, longest_dry_spell_name = longest_dry_spell_name, spell_length_name = spell_length_name, na.rm = na.rm, replace = replace, month_col_names = month_col_names, summary_col_names = summary_col_names)
       # date time period is "yearly"
       data_list = add_to_data_info_time_period(data_list, yearly_label)
-      print("1ii")
-      View(data_list)
       # a list of climate data objects
       climate_data_objs = get_climate_data_objects(data_list)
-      print("2")
-    
-	  for(data_obj in climate_data_objs){
-	      curr_data_list = data_obj$get_data_for_analysis(data_list)
-	      print("2ii")
-		  for( curr_data in curr_data_list ) {
+      	  for(data_obj in climate_data_objs){
+	            curr_data_list = data_obj$get_data_for_analysis(data_list)
+	      
+		          for( curr_data in curr_data_list ) {
 		     
-            for (i in 1:length(summary_col_names) ){
-		            for (j in  1:length(month_col_names)){
-		                names(curr_data)[names(curr_data) == paste(month_col_names[[j]],summary_col_names[[i]])] <-paste(summary_col_names[[i]], month_col_names[[j]], sep=" ")
+                  for (i in 1:length(summary_col_names) ){
+		                  for (j in  1:length(month_col_names)){
+		                      names(curr_data)[names(curr_data) == paste(month_col_names[[j]])] <-paste(summary_col_names[[i]], month_col_names[[j]], sep=" ")
 		            }
 		        }
-            View(curr_data)
-          #curr_data<-subset(curr_data, select=c("Year","Total Rainfall","Total Rainfall_SeasonA"))
+            tmp_data<-subset(curr_data, select=c("Year","Total Rainfall","Total Rainfall_SeasonA"))
   	    #extracting the yearly summaries.
 	      #names(curr_data)<-c( "Year","TotalRainfall","TotalRainfall_SeasonA","TotalRainfall_SeasonB",
 		     #   "SeasonStart_A","SeasonStart_B","SeasonEnd_A","SeasonEnd_B","LengthOfSeason_A",
 		      #  "LengthOfSeason_B","MinTemperature","MaxTemperature")
-	      print("2iii")
-        write.csv(curr_data, file=Sitename.csv,sep = ",",column.names=T, row.names = F,quote = F)
+	      write.csv(tmp_data, file=Sitename.csv,sep = ",",column.names=T, row.names = F,quote = F)
         }  
 	    }
   }
